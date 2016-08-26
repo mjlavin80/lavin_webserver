105d7c63cc606b47a82c2506f740b76719b971d6
matthewlavin <matthewlavin@backups-MacBook-Air-3.local>
2016-08-26 14:45:21 -0400

    no db handler flask

--- digiped_fall_2016/app/main.py
+++ digiped_fall_2016/app/main.py
@@ -1,29 +1,27 @@
 from flask import Flask
 import os
-
+import sqlalchemy
+from config import dbuser, dbpass
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    dbuser = 'root'
-    dbpass = 'goblin55'
-    dbaddr = os.getenv('DB_PORT_3306_TCP_ADDR')
-    #dbdb   = os.getenv('DB_ENV_MYSQL_DATABASE','flask')
-
-    uri = "mysql://%s:%s@mysql:3306/digiped_fall_2016" % (dbuser, dbpass)
-
-    import sqlalchemy
-    engine = sqlalchemy.create_engine(uri) # connect to server
-
     try:
+        uri = "mysql://%s:%s@mysql:3306/digiped_fall_2016" % (dbuser, dbpass)
+        engine = sqlalchemy.create_engine(uri) # connect to server
         a = engine.execute("SELECT * FROM test")
         test = ""
         for i in a:
             test += str(i)
         return test
-
-    except Exception, e:
-        return repr(e)
+    except:
+        try:
+            uri = "mysql://%s:%s@mysql:3306" % (dbuser, dbpass)
+            engine = sqlalchemy.create_engine(uri) # connect to server
+            a = engine.execute("CREATE DATABASE digiped_fall_2016 CHARACTER SET='utf8' COLLATE='utf8_general_ci'")
+            return "created database. Reload to see contents."
+        except Exception, e:
+            return repr(e)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
