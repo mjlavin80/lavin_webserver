28f354954c301abd545ff821f4ed31d2e8bf3d24
Lavin <lavin@LavinMacPro.local>
2016-08-29 17:22:57 -0400

    templates and content

--- courses/digiped_fall_2016/app/main.py
+++ courses/digiped_fall_2016/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 import os
 from application.models import *
 from application import db
@@ -7,9 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World from Flask"
+    return render_template("index.html")
 
-#this section needs a layer of protection so a user can't resest the db
+@app.route("/policies")
+def policies():
+    return render_template("policies.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+@app.route("/readings")
+def readings():
+    return render_template("readings.html")
+
+@app.route("/bibliography")
+def biblio():
+    return render_template("bibliography.html")
+
+#this section needs a layer of protection so a user can't reset the db
 """
 @app.route("/setup")
 def setup():
@@ -22,7 +42,7 @@ def setup():
 
 if __name__ == "__main__":
     #for local dev
-    #app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
     #for production
-    app.run(host='0.0.0.0', port=80)
+    #app.run(host='0.0.0.0', port=80)
