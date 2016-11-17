8337acbca40c480e1d66023b5cec19bcc0268abf
Matt Lavin <lavin@pitt.edu>
2016-11-17 14:14:25 -0500

    debugging

--- courses/acdm_spring_2017/app/main.py
+++ courses/acdm_spring_2017/app/main.py
@@ -202,4 +202,4 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
 
     #for dev
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    #app.run(host='0.0.0.0', debug=True, port=5000)
