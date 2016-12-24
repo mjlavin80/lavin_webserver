ad2028035359951c052e02d2c49542cdabbe7e18
mjlavin80 <lavin@pitt.edu>
2016-12-24 14:07:02 -0500

    core

--- courses/acdm_spring_2017/app/main.py
+++ courses/acdm_spring_2017/app/main.py
@@ -105,7 +105,10 @@ def generate_site_data():
 def include_site_data(fn):
     @app.context_processor
     def additional_context():
+        #site_basics
         basics = generate_site_data()
+        #user_authorization
+        
         return {"basics":basics}
     return fn
 
