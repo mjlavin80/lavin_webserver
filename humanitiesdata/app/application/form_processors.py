4c77e70441c83cab9bf054838cafa77237e3d4bc
mjlavin80 <lavin@pitt.edu>
2017-01-03 12:33:38 -0500

    edit refactored debugged

--- humanitiesdata/app/application/form_processors.py
+++ humanitiesdata/app/application/form_processors.py
@@ -35,11 +35,11 @@ def handle_tags_on_edit(tag_list, ins):
             return "len_error"
     for old in ins.tags:
         ins.tags.remove(old)
-        db.session.commit()
-    # try:
-    #     db.session.commit()
-    # except:
-    #     db.session.rollback()
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            
     for a_tag in suggested_tags:
         #check for tag in db
         newtag = Tag.query.filter(Tag.tagname==a_tag).one_or_none()
