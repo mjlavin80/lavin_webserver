6f8086724e25af01896c32653e48c9a651f2cdf8
Matt Lavin <lavin@pitt.edu>
2017-07-04 12:14:01 -0400

    compvolution ssl in prog

--- compvolution/app/main.py
+++ compvolution/app/main.py
@@ -358,6 +358,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
     #for production
     #app.run(host='0.0.0.0', port=port)
-
+    app.run(port=443, ssl_context=('cert.pem', 'key.pem'))
     #for dev
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    #app.run(host='0.0.0.0', debug=True, port=5000)
