bdef34e193cdc82a8ef046b643ba97df89ea1669
mjlavin80 <lavin@pitt.edu>
2017-07-04 12:56:20 -0400

    logout

--- compvolution/app/main.py
+++ compvolution/app/main.py
@@ -317,7 +317,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
-    return redirect(url_for('status'))
+    return redirect(url_for('index'))
 
 @app.route('/status')
 def status(message=""):
