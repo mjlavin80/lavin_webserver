08d6d1f5ab5fc6882bdc69d4220a2c10fad9aa88
Matthew Lavin <lavin@pitt.edu>
2018-01-12 15:58:33 -0500

    debug

--- courses/cdm_spring_2018/app/main.py
+++ courses/cdm_spring_2018/app/main.py
@@ -63,9 +63,11 @@ class ModelViewUser(ModelView):
 
 class ModelViewAdmin(ModelView):
     column_formatters = dict(course_description=lambda v, c, m, p: m.course_description[:25]+ " ...", description=lambda v, c, m, p: m.description[:25]+ " ...")
+
     form_overrides = dict(description=TextAreaField, course_description=TextAreaField)
 
     form_widget_args = dict(description=dict(rows=10), course_description=dict(rows=10))
+
     def is_accessible(self):
         if current_user.is_authenticated and current_user.is_admin:
             return current_user.is_authenticated
