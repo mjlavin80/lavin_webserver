d8e3d84611bfb27ce3ac6141c2b4710ad92562de
mjlavin80 <lavin@pitt.edu>
2018-01-13 13:46:53 -0500

    app fix for old sites

--- courses/mtb_fall_2017/app/main.py
+++ courses/mtb_fall_2017/app/main.py
@@ -545,6 +545,8 @@ def server_error(e):
 def gateway_error(e):
     return render_template('500.html'), 502
 
+db.init_app(app)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
     #for production
