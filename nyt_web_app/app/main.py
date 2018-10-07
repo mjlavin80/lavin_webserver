d21ec98c640f1d614e23923fd69c7bd40a44c2d1
mjlavin80 <lavin@pitt.edu>
2018-10-07 12:15:03 -0400

    page range

--- nyt_web_app/app/main.py
+++ nyt_web_app/app/main.py
@@ -124,7 +124,10 @@ def index(nyt_id=None):
 
             return render_template("index.html", nyt_id=nyt_id, row=row, endpoint=endpoint, pdf_link=pdf_link)
         else:
-            row = Metadata().query.filter(and_(Metadata.year > 1905, Metadata.year < 1923)).filter(Metadata.review_type.like("needs_audit%")).filter(Metadata.page == 'BR2').order_by(func.rand()).first()
+            row = Metadata().query.filter(and_(Metadata.year > 1905, Metadata.year < 1923)).filter(Metadata.review_type.like("needs_audit%")).order_by(func.rand()).all()
+            pages = ['BR3', 'BR4', 'BR5', 'BR6', 'BR7', 'BR8', 'BR9', 'BR10']
+            row = [i for i in row if i.page in pages]
+            row = row[0]
             # BR1, 'BR2', 'BR3', 'BR4', 'BR5', 'BR6', 'BR7', 'BR8', 'BR9', 'BR10'
             endpoint = row.nyt_pdf_endpoint
             r = requests.get(endpoint)
