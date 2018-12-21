4226ccfd11a7207646a2eac29efd5d1ae13b8d28
Matt Lavin <lavin@pitt.edu>
2018-12-21 15:58:32 -0500

    proxy fixes

--- courses/pdc_spring_2019/app/application/forms.py
+++ courses/pdc_spring_2019/app/application/forms.py
@@ -5,31 +5,7 @@ from wtforms_alchemy import ModelFieldList, model_form_factory
 from wtforms.fields import FormField
 from wtforms.widgets import TextArea
 
-
 ModelForm = model_form_factory(Form)
-
-class LoginForm(Form):
-    username = TextField('Username', [validators.required()])
-    password = PasswordField('Password', [validators.required()])
-
-class CreateActivity(ModelForm):
-    class Meta:
-        model = Activity
-    public =  SelectField('Privacy', choices = [(1,"Private"), (2, "Public")],  coerce=int)
-
-class CreateAssignment(ModelForm):
-    class Meta:
-        model = Assignment
-    public =  SelectField('Privacy', choices = [(1,"Private"), (2, "Public")], coerce=int)
-class CreateReading(ModelForm):
-    class Meta:
-        model = Reading
-    public =  SelectField('Privacy', choices = [(1,"Private"), (2, "Public")], coerce=int)
-
-class CreateCollection(ModelForm):
-    class Meta:
-        model = Collection
-    public =  SelectField('Privacy', choices = [(1,"Private"), (2, "Public")], coerce=int)
 
 class CKEditor(TextArea):
     def __call__(self, field, **kwargs):
