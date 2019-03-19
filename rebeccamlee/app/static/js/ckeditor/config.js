e330e037870de69886ace5cd1ad88b68e5ddc45f
rebeccamlee <46057004+rebeccamlee@users.noreply.github.com>
2019-03-19 18:23:04 -0400

    Update config.js

--- rebeccamlee/app/static/js/ckeditor/config.js
+++ rebeccamlee/app/static/js/ckeditor/config.js
@@ -35,4 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	
+	// Allow div class= on pages
+	config.allowedContent = true;
 };
