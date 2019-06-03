2627f31372f6aa4f78757913f72e4e0c25af0cf9
mjlavin80 <lavin@pitt.edu>
2019-06-03 16:29:16 -0400

    ckeditor

--- rebeccamlee/app/static/js/ckeditor/config.js
+++ rebeccamlee/app/static/js/ckeditor/config.js
@@ -4,7 +4,8 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
+	config.allowedContent = true;
+        // Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
