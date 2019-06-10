8c823334894afeca9fc3992086d2dd5139bbffbe
mjlavin80 <lavin@pitt.edu>
2019-06-10 11:41:24 -0400

    fixing big font

--- matthew-lavin/app/static/js/ckeditor/config.js
+++ matthew-lavin/app/static/js/ckeditor/config.js
@@ -4,6 +4,7 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+	config.allowedContent = true;
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
