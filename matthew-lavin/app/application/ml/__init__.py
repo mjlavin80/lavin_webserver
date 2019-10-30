a38fb8ef3723a67f0d263c5b871f71199282df5a
mjlavin80 <lavin@pitt.edu>
2019-10-30 18:33:37 -0400

    workshops link

--- matthew-lavin/app/application/ml/__init__.py
+++ matthew-lavin/app/application/ml/__init__.py
@@ -14,3 +14,9 @@ def teaching():
 def projects():
     data = StaticPage.query.filter(StaticPage.route == "projects").one_or_none()
     return render_template('projects.html', data=data)
+
+@ml_blueprint.route("/workshops")
+@ml_blueprint.route("/workshops/")
+def projects():
+    data = StaticPage.query.filter(StaticPage.route == "workshops").one_or_none()
+    return render_template('workshops.html', data=data)
