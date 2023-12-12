358e76744bd5631de2478ec8c52ffa3db64a5895
Matthew Lavin <matthewlavin@Matthews-MacBook-Pro-3.local>
2023-12-12 15:08:27 -0500

    bugfix

--- humanities_analytics/app/application/courses/courses.py
+++ humanities_analytics/app/application/courses/courses.py
@@ -47,7 +47,7 @@ def load_course(semester=None, course=None, template=None):
 @courses_blueprint.route("/da-210/<semester>")
 @courses_blueprint.route("/da-210/<semester>/")
 def da_210(semester=None, course=None, template=None):
-    return load_course(semester=None, course="da-210", template="syllabus.html")
+    return load_course(semester=semester, course="da-210", template="syllabus.html")
 
 @courses_blueprint.route("/da-245")
 @courses_blueprint.route("/da-245/")
@@ -90,8 +90,8 @@ def da_350(semester=None):
 @courses_blueprint.route("/da-351/<semester>")
 @courses_blueprint.route("/da-351/<semester>/")
 def da_351(semester=None, course=None, template=None):
-    return load_course(semester=None, course="da-351", template="syllabus.html")
-    
+    return load_course(semester=semester, course="da-351", template="syllabus.html")
+
 @courses_blueprint.route("/da-401")
 @courses_blueprint.route("/da-401/")
 @courses_blueprint.route("/da-401/<semester>")
